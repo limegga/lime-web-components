1103dd1ebb29bbb45ecf88014831c5f32369140d
Adrian Schmidt <adrian.schmidt@lime.tech>
2020-03-30 16:34:11 +0200

    ci(release): add commit author

--- release.sh
+++ release.sh
@@ -3,6 +3,9 @@ git remote rm origin
 git remote add origin "https://${GH_TOKEN}@github.com/lundalogik/lime-web-components.git"
 git push --set-upstream origin master
 
+git config --global user.email "${AUTHOR_EMAIL}"
+git config --global user.name "${AUTHOR_NAME}"
+
 npm run build
 
 cat << EOL >> ".npmrc"
