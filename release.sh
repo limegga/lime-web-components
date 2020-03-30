87311e7378b6357cb936c33ecf225d0ce9f70be6
Adrian Schmidt <adrian.schmidt@lime.tech>
2020-03-30 16:40:54 +0200

    ci(release): fix commit author variables

--- release.sh
+++ release.sh
@@ -3,8 +3,8 @@ git remote rm origin
 git remote add origin "https://${GH_TOKEN}@github.com/lundalogik/lime-web-components.git"
 git push --set-upstream origin master
 
-git config --global user.email "${AUTHOR_EMAIL}"
-git config --global user.name "${AUTHOR_NAME}"
+git config --global user.email "${GIT_AUTHOR_EMAIL}"
+git config --global user.name "${GIT_AUTHOR_NAME}"
 
 npm run build
 
