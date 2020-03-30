306e7a2c0eaeda4006f3a4dde0912be7611fb272
Adrian Schmidt <adrian.schmidt@lime.tech>
2020-03-30 17:06:11 +0200

    Revert "ci(release): graduate pre-releases"
    This reverts commit cb841073cc87ea6dadcd98ff27fbf3f9e6c31aee.

--- release.sh
+++ release.sh
@@ -13,7 +13,7 @@ registry=https://registry.npmjs.org/
 //registry.npmjs.org/:_authToken=${NPM_TOKEN}
 EOL
 
-npx lerna version --loglevel=DEBUG --conventional-commits --conventional-graduate --create-release=github --yes
+npx lerna version --loglevel=DEBUG --conventional-commits --create-release=github --yes
 npx lerna --loglevel=INFO publish from-git --yes
 
 rm .npmrc
