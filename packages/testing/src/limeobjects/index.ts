7389723dcce5420ba20f0a8c584c1e8da35fdde3
Johan Groth <johan.groth@lime.tech>
2020-04-20 13:42:58 +0200

    feat: add test fixtures for limeobjects

---
+++ packages/testing/src/limeobjects/index.ts
@@ -0,0 +1,27 @@
+import { company } from './company';
+import { coworker } from './coworker';
+import { deal } from './deal';
+import { person } from './person';
+
+export const limeobjects = {
+    company: company,
+    coworker: coworker,
+    person: person,
+    deal: deal,
+    solutionimprovement: [],
+    mailing: [],
+    history: [],
+    recipient: [],
+    helpdesktype: [],
+    successplan: [],
+    document: [],
+    consenttype: [],
+    infotiles: [],
+    participant: [],
+    campaign: [],
+    todo: [],
+    helpdesk: [],
+    office: [],
+    target: [],
+    consent: [],
+};
